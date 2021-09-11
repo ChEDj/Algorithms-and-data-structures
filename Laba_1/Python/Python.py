c3112aa3777a9af88d9d75e054e284d7b294d7d1
Edem <edemcherkezov@gmail.com>
2021-09-11 15:59:53 +0300

    add all

--- Laba_1/Python/Python.py
+++
@@ -1,7 +0,0 @@
-import math
-rad = float(input("Enter radius of the circle: "))
-pi = 3.14
-C = 2 * pi * rad
-S = pi * math.pow(rad, 2)
-print("Length of the circle =", C)
-print("Area of the circle =", S)

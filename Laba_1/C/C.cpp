25b67b216b9f11f809edbd6a2642e732af7bd3a3
Edem <edemcherkezov@gmail.com>
2021-09-11 13:52:51 +0300

    add all

---
+++ Laba_1/C/C.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float rad;
+    float pi = 3.14;
+    cout << "Enter radius of the circle:" << endl;
+    cin >> rad;
+    float C = 2 * pi * rad;
+    float S = pi * pow(rad, 2);
+    cout << "Length of the circle = " << C << endl;
+    cout << "Area of the circle = " << S << endl;
+}
---
+++ Laba_1/Python/Python.py
@@ -0,0 +1,7 @@
+import math
+rad = float(input("Enter radius of the circle: "))
+pi = 3.14
+C = 2 * pi * rad
+S = pi * math.pow(rad, 2)
+print("Length of the circle =", C)
+print("Area of the circle =", S)
